29d38062bea8f1bdcabf315170941142d1cec4ad
Yusuf Arslan <yusufarslantubitak@gmail.com>
2026-06-02 15:31:05 +0300

    chore: refactor code structure and create base for the homepage

--- homepage/vite.config.ts
+++ homepage/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   server: {
     host: '127.0.0.1',
     port: 5173,
@@ -12,6 +13,7 @@ export default defineConfig({
   resolve: {
     alias: {
       'react-zeugma': path.resolve(__dirname, '../src'),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
